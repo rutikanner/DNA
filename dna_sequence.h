368f3cf481e17f905c056adff6004cda411627c3
rutikanner <rk0548447845@gmail.com>
2020-06-01 05:40:10 -0700

    Without exeptions

--- dna_sequence.h
+++ dna_sequence.h
@@ -5,12 +5,84 @@
 #ifndef DNASEQUENCE_DNA_SEQUENCE_H
 #define DNASEQUENCE_DNA_SEQUENCE_H
 #include <string>
+#include <iostream>
 #include <cstring>
+
 
 class DnaSequence{
 public:
-    DnaSequence(std::string str);
-//    DnaSequence(string str)
+    DnaSequence(std::string string);
+//    DnaSequence(char* string);
+    DnaSequence(DnaSequence& dnaSequence);
+    DnaSequence& operator = (const DnaSequence& dnaSequence);
+//    DnaSequence& operator = (const char* string);
+    DnaSequence& operator = (const std::string& string);
+    char operator [] (size_t index);
+    void init(const char* dna);
+    char* getDna() const;
+    size_t length(const char *& string) const ;
+
+private:
+    char* m_dna;
+
 };
+
+
+inline DnaSequence::DnaSequence(std::string string) : m_dna(new char [string.length()]) {
+    strcpy(m_dna, string.c_str());
+}
+//
+//inline DnaSequence::DnaSequence(char *string) : m_dna(new char [strlen(string)]) {
+//    strcpy(m_dna, string);
+//}
+
+inline DnaSequence::DnaSequence(DnaSequence &dnaSequence) : m_dna(new char [strlen(dnaSequence.getDna())]){
+    strcpy(m_dna, dnaSequence.getDna());
+}
+
+inline void DnaSequence::init(const char *dna) {
+    char* tmp = new char [strlen(dna)];
+    strcpy(tmp , dna);
+    delete [] m_dna;
+    m_dna = tmp;
+}
+
+inline DnaSequence & DnaSequence::operator=(const DnaSequence &dnaSequence) {
+    init(dnaSequence.m_dna);
+    return *this;
+}
+
+inline DnaSequence&  DnaSequence::operator=(const std::string& dna) {
+    init(dna.c_str());
+    return *this;
+}
+
+//inline DnaSequence & DnaSequence::operator=(const char* dna) {
+//    init(dna);
+//    return *this;
+//}
+
+inline char* DnaSequence::getDna() const {return m_dna;}
+
+inline std::ostream& operator << (std::ostream& os, const DnaSequence& dnaSequence)
+{
+    os<<dnaSequence.getDna();
+    return os;
+}
+
+inline int operator == (const DnaSequence& dnaSequence1, const DnaSequence& dnaSequence2)
+{
+    return dnaSequence1.getDna()==dnaSequence2.getDna();
+}
+
+inline int operator != (const DnaSequence& dnaSequence1, const DnaSequence& dnaSequence2)
+{
+    return !(dnaSequence1.getDna()==dnaSequence2.getDna());
+}
+
+inline size_t DnaSequence::length(const char *& string) const
+{
+    return strlen(string);
+}
 
 #endif //DNASEQUENCE_DNA_SEQUENCE_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include "dna_sequence.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    char* c_str= (char *)"ruti kanner";
+    std::string string = "shoshi rizel";
+    DnaSequence d1((char*) "ruti");
+    std::cout<<d1<<std::endl;
+//    DnaSequence d2("kanner");
+//    std::cout<<d2<<std::endl;
+    DnaSequence d3(d1);
+    std::cout<<d3<<std::endl;
+    //d3 = d2;
+    std::cout<<d3<<std::endl;
+    d1 = c_str;
+    std::cout<<d1<<std::endl;
+//    d2 = string;
+//    std::cout<<d2<<std::endl;
+    DnaSequence d4(c_str);
+    std::cout<<d4<<std::endl;
+    DnaSequence d5(string);
+    std::cout<<d5<<std::endl;
+
     return 0;
 }

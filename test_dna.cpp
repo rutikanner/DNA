c4f4f8578f7fa360bcd83f4b9c43e878c4f87ee8
rutikanner <rk0548447845@gmail.com>
2020-06-06 14:38:32 -0700

    add test

--- test_dna.cpp
+++ test_dna.cpp
@@ -1,25 +1,26 @@
-////
-//// Created by ruti on 6/4/20.
-////
 //
-//#include "test_dna.h"
-//#include <cstring>
-//#include "dna_sequence.h"
+// Created by ruti on 6/4/20.
 //
-//
-///////////////////////////////////////
-//// TESTS under this line
-//
-//TEST(StackBasicTests, EmptyStack)
-//{
-//    DnaSequence d1((char *)"CG");
-//    ASSERT_TRUE(s.isEmpty());
-//}
-//
-//TEST(StackBasicTests, PushOneToStack)
-//{
-//    Stack<int> s;
-//    EXPECT_TRUE(s.isEmpty());
-//    s.push(5);
-//    ASSERT_FALSE(s.isEmpty());
-//}
+
+#include "test_dna.h"
+#include <cstring>
+#include "dna_sequence.h"
+
+
+/////////////////////////////////////
+// TESTS under this line
+
+TEST(DnaTest, OperatorEqual)
+{
+    DnaSequence d1((char*)"CG");
+    DnaSequence d2((char*)"CG");
+    ASSERT_TRUE(d1.getDna() == d2.getDna());
+}
+
+
+TEST(DnaTest, OperatorNotEqual)
+{
+    DnaSequence d1((char*)"CG");
+    DnaSequence d2((char*)"C");
+    ASSERT_TRUE(d1.getDna() != d2.getDna());
+}
